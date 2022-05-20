3616bf0649fb72c7f093bf697ecabddc279808d5
Julie <jatkin80@ford.com>
2022-05-20 12:31:09 -0500

    router links active

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,8 +1,12 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   selector: "app-about-page",
   templateUrl: "./about-page.component.html",
   styleUrls: ["./about-page.component.css"]
 })
-export class AboutPageComponent {}
+export class AboutPageComponent implements OnInit{
+ngOnInit(): void {
+
+}
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsPageComponent } from './products-page/products-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductListingComponent } from './product-listing/product-listing.component';
-import { AppComponent } from './app.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [{
-  path: " ",
-  component: AppComponent,
-}, {
+  path: "home",
+component: HomePageComponent,
+},
+{ path: '', redirectTo: '/home', pathMatch: 'full' },{
   path: "products",
   component: ProductsPageComponent,
 }, {
@@ -16,7 +18,10 @@ const routes: Routes = [{
   component: ProductPageComponent,
 }, {
   path: "about",
-  component: ProductListingComponent,
+  component: AboutPageComponent,
+},{
+ path: "productListing",
+ component: ProductListingComponent,
 }];
 
 @NgModule({
